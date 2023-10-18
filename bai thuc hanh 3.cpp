defd9e8a500e3a47a56f9f5c07249a7eb7b398c7
Kiet3tuoi <147957146+Kiet3tuoi@users.noreply.github.com>
2023-10-18 15:43:36 +0700

    Add files via upload

---
+++ bai thuc hanh 3.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	printf("\n Nhap gia tri cua a:");
+	scanf("%d", &a);
+	printf("\n Nhap gia tri cua b:");
+	scanf("%d", &b);
+	printf("\n nhap gia tri cua c:");
+	scanf("%d", &c);
+	{
+	
+    printf("\n Tong hai so la: %d", a+b+c);
+	printf("\n Tich hai so la: %d", a*b*c);
+	return 0;
+}
