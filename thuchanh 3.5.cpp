db1626317caa960f35dd69e91f19fedc52e54bea
Kiet3tuoi <147957146+Kiet3tuoi@users.noreply.github.com>
2023-10-25 09:26:10 +0700

    Add files via upload

---
+++ thuchanh 3.5.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("\n Nhap so ngay tuong ung: ");
+	scanf("%d", &n);
+   {
+   
+        if(n==1)
+	    printf("\n 1: Sunday", n);
+	     if(n==2)
+	    printf("\n 2: Tuesday ", n);
+	     if(n==3)
+	    printf("\n 3: Wednesday", n);
+	     if(n==4)
+	    printf("\n 4: Thursday", n);
+	     if(n==5)
+	    printf("\n 5: Friday", n);
+	     if(n==6)
+	    printf("\n 6: Saturday", n);
+	     if(n==7)
+	    printf("\n 7: Sunday", n);
+    }
+    return 0;
+}
