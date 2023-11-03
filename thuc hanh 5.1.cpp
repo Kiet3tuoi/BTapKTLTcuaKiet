347bfd4d7c4998d215dd96239b82cf62c0514e51
Kiet3tuoi <147957146+Kiet3tuoi@users.noreply.github.com>
2023-11-03 07:51:53 +0700

    Add files via upload

---
+++ thuc hanh 5.1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char s1[50];
+	char s2[50];
+	printf("\n Nhap ky tu s1:");
+    scanf("%s", &s1);
+	printf("\n Nhap ky tu s2:");
+	scanf("%s",&s2);
+	printf("\n Chuoi ky tu vua nhap la: %s %s", s1,s2);
+	return 0;
+}
